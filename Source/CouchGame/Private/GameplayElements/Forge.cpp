dc0997890a34f11eac9659794ba32ab4440792f5
Jari Bou <bourdietome@gmail.com>
2024-10-23 14:36:09 +0200

    [ADD] Forge

---
+++ Source/CouchGame/Private/GameplayElements/Forge.cpp
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameplayElements/Forge.h"
+
+
+// Sets default values
+AForge::AForge()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AForge::SpawnWeaponsAtRandomLocation(UPrimaryDataAsset* WeaponData, int Number)
+{
+	// CreateWeapon
+}
+
+// Called when the game starts or when spawned
+void AForge::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AForge::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
