1bf06d2563a5e1afc391b2ede28f52b1e519482f
Clément THOMAS <clemto95@live.fr>
2024-11-12 14:42:09 +0100

    [ADD] Muddy Ground

---
+++ Source/CouchGame/Public/GameplayElements/MuddyGround.h
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Characters/SfCharacter.h"
+#include "GameFramework/Actor.h"
+#include "MuddyGround.generated.h"
+
+UCLASS()
+class COUCHGAME_API AMuddyGround : public AActor
+{
+	GENERATED_BODY()
+#pragma region Unreal Default
+public:
+	// Sets default values for this actor's properties
+	AMuddyGround();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+#pragma endregion
+
+#pragma region Mud
+
+public:
+	UFUNCTION()
+	void BeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+	
+
+protected:
+	
+	UPROPERTY(EditAnywhere)
+	float SlowPercent = 0.f;
+
+	UPROPERTY(EditAnywhere)
+	float Duration = 0.f;
+	
+private:
+	UPROPERTY()
+	TObjectPtr<UStaticMeshComponent> MuddyGroundMesh;
+
+	TMap<TObjectPtr<ASfCharacter>, float> OverlappingActorsAndSpeedOnEnter;
+	
+#pragma endregion
+};
