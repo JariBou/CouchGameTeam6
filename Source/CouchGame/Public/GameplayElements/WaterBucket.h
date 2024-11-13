5470b79fa8b77fc220204ddbad9e53214ffaca86
Clément THOMAS <clemto95@live.fr>
2024-11-13 10:10:52 +0100

    [ADD] WaterBucket (Not tested yet)

---
+++ Source/CouchGame/Public/GameplayElements/WaterBucket.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Pickable.h"
+#include "WaterBucket.generated.h"
+
+UCLASS()
+class COUCHGAME_API AWaterBucket : public APickable
+{
+	GENERATED_BODY()
+
+#pragma region Unreal Default
+	
+public:
+	// Sets default values for this actor's properties
+	AWaterBucket();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+		
+#pragma endregion
+
+#pragma region Bucket
+
+public:
+	UFUNCTION()
+	void ComponentHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+	
+protected:
+	bool IsFilled = false;
+
+	void SpawnMuddyGround(FVector Location, FRotator Rotation);
+private:
+
+#pragma endregion
+};
