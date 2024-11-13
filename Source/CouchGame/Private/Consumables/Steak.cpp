0b40f4dd08883f96be5526bccbac162f74077eee
Grm <jeremylombard.mail@gmail.com>
2024-11-13 17:29:14 +0100

    Consumables TEST DONT MERGE

---
+++ Source/CouchGame/Private/Consumables/Steak.cpp
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Consumables/Steak.h"
+
+
+// Sets default values
+ASteak::ASteak()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	ChangingStat = ETypeOfChangingStat::HEALTH;
+	ChangingStatValue = 10.f;
+}
+
+// Called when the game starts or when spawned
+void ASteak::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASteak::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/CouchGame/Public/Consumables/Steak.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Consumables.h"
+#include "Steak.generated.h"
+
+UCLASS()
+class COUCHGAME_API ASteak : public AConsumables
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ASteak();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
