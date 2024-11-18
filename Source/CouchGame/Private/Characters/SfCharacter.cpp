83b4e7bfe36bdba1f2ba1acd92f98ae465d47517
Jari Bou <bourdietome@gmail.com>
2024-11-18 12:02:01 +0100

    fix?

--- Source/CouchGame/Private/Characters/SfCharacter.cpp
+++ Source/CouchGame/Private/Characters/SfCharacter.cpp
@@ -105,7 +105,7 @@ void ASfCharacter::EndPlay(const EEndPlayReason::Type EndPlayReason)
 {
 	Super::EndPlay(EndPlayReason);
 
-	Drop();
+	if (IsCarrying) Drop();
 
 	GetWorld()->GetSubsystem<UCameraWorldSubsystem>()->RemoveFollowTarget(this);
 }
