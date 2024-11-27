1e69aae44d6f69f997905d12465ad28fd0d855aa
Jari Bou <bourdietome@gmail.com>
2024-11-27 16:12:18 +0100

    start

---
+++ Source/CouchGame/Public/Systems/CharacterSelectionSubsystem.h
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include <Teams.h>
+
+#include "CoreMinimal.h"
+#include <Subsystems/GameInstanceSubsystem.h>
+#include "CharacterSelectionSubsystem.generated.h"
+
+USTRUCT()
+struct FPlayerSelectionInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY()
+	APlayerController* PlayerController;
+
+	UPROPERTY()
+	TEnumAsByte<ETeam> PlayerTeam; 
+	
+	
+};
+
+/**
+ * 
+ */
+UCLASS()
+class COUCHGAME_API UCharacterSelectionSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+	
+
+	private:
+	UPROPERTY(EditAnywhere)
+	TMap<uint8_t, FPlayerSelectionInfo> m_playerIdToInfoMap;
+};
